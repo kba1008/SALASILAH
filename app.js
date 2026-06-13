e4da6932036ab41cf33b8749c9d7e03cebd25b6b
kba1008 <sekolah-4896@moe-dl.edu.my>
2026-06-13 22:07:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 // ====== KONFIGURASI ======
 // 🔗 Tampal URL Web App Google Apps Script anda di sini:
-const API_URL = "https://script.google.com/macros/s/AKfycbySnlDlLMvubSyGg9zrQ6KbGpH76gM38-HRk4z_GqX1rH6HK_fGQPVGNkRRUwBg7unn/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbx3CH2DfMIq6iv61SojuboWv42_56jsGSqd_-RPsKW45x6MF7CvTrwujNt2kxQRx41W/exec";
 
 // 📞 Talian / WhatsApp pentadbir untuk pengesahan maklumat salasilah.
 const ADMIN_PHONE = "01110661077";
