34782932c0797ef4e389b419d9955c29ce9263d3
kba1008 <sekolah-4896@moe-dl.edu.my>
2026-06-13 09:51:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 // ====== KONFIGURASI ======
 // 🔗 Tampal URL Web App Google Apps Script anda di sini:
-const API_URL = "https://script.google.com/macros/s/AKfycbz-AgLGfoA55OvnXULYtAnbg1e6AqQmGJ38JX-CymaedAkgG2tfOTHsDEYr8EoN94br/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwcnkQc_gyuWKiQR_n2Ae8p0S8B-j_dMBT0yHyNjEpG9a3v_1Z6ya-6Oy2z53P-MUEM/exec";
 
 const $ = (s, r=document) => r.querySelector(s);
 const $$ = (s, r=document) => Array.from(r.querySelectorAll(s));
